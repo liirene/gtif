f4da042c68e6fcdd78bf1bfc00e430c8dab4de41
Irene Li <ireneli@DN0a22f77e.SUNet>
2017-04-17 11:31:42 -0700

    initial commit

---
+++ parse_webpage.py
@@ -0,0 +1,17 @@
+import beautifulsoup
+
+# http://www.scimagojr.com/journalrank.php
+
+### User Input
+
+# Have an area to randomize
+# Can input Subject area (dropdown fill?)
+# Include year (2015 down)
+
+# Pull random (how to do this?)
+
+# Round to the nearest 100th place
+
+# Math calculations
+# see whether it's close? Within 0.5 pt?
+   
