7a483e74613923df0ca1c2c58d33ad9808f09063
Irene Li <ireneli@DN0a62f774.SUNet>
2017-04-24 15:58:19 -0700

    OOPs.

--- parse_webpage.py
+++ parse_webpage.py
@@ -1,10 +1,27 @@
-from bs4 import beautifulsoup
+from bs4 import BeautifulSoup
 import requests
+import sys
+import re
 
 # http://www.scimagojr.com/journalrank.php
-page = requests.get()
+
+url = "http://www.scimagojr.com/journalrank.php"
+page = requests.get(url)
+soup = BeautifulSoup(page.content, 'html.parser')
+
+def Scrape(year: int, subject: str, metric: str):
+    # Function to scrape the data and input it to the dictionary
+
+
+
+# Build method of pulling a random one (memoized dictionary?) from parameters assigned
+
+# First build dictionary of all ??
 
 ### User Input
+year = input('Please input a year (currently only 2015 is supported')
+subjectarea = input('Please input a subject area. Leaving this blank will pull from all subjects randomly')
+metrix = input('Please input a metric: SJR (s), total citations (t)')
 
 # Have an area to randomize
 # Can input Subject area (dropdown fill?)
